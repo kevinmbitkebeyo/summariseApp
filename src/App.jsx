4c6a3c4de69739f4589f82e170b5ed307e8f43c4
Kevin Mbitkebeyo <mbitkebeyokevin@gmail.com>
2023-07-14 07:11:28 +0200

    implement demo summarize and fetch api of summarize

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import "./App.css";
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
+
+const App = () => {
+  return <main>
+    <div className="">
+        <div className="gradient" />
+    </div>
+    <div className="app">
+        <Hero/>
+        <Demo />
+    </div>
+  </main>;
+};
+
+export default App;
