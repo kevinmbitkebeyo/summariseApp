f48413544095fdc6cfe5871ee009b5b42fe35353
Kevin Mbitkebeyo <mbitkebeyokevin@gmail.com>
2023-08-04 17:18:56 +0200

    update hero jsx file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
-import { logo } from "../assets";
+import { logo, copy } from "../assets";
 
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center flex-col">
-        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <img src={copy} alt="sumz_logo" className="w-28 object-contain" />
         <button
           type="button"
           onClick={() => window.open("https://github.com")}
@@ -15,7 +15,7 @@ const Hero = () => {
       </nav>
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden" />
-        <span className="blue_gradient ">OpenAI GPT-4</span>
+        <span className="blue_gradient ">Summarise GPT</span>
       </h1>
       <h2 className="desc">
         Simplify your reading with Summize, an open-source article summarizer
